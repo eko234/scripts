077044d802df4abf8700fc2c8757d4a4b5952791
jujo <jujo@pop-os.localdomain>
2022-02-07 00:36:24 -0500

    fixie

--- chandl.sh
+++ chandl.sh
@@ -4,9 +4,7 @@ if [ $# -ne 2 ]; then
   exit 1
 fi
 
-command=\
-'sfileThumb<ret>/href....i<ret><semicolon>t"d'\
-'<percent><a-d><a-R>li<ret>,<backspace><esc>,d'
+command='sfileThumb<ret>/href....i<ret><semicolon>t"d<percent><a-d><a-R>li<ret>,<backspace><esc>,d'
 
 wget -q -O - $2 |
 kak -f $command |
